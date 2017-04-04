10b3dc7d21ed843e3885de994a6281593d5b14bd
Francois Best <francoisbest@gmail.com>
2017-04-04 09:58:35 +0200

    Removed CMake sources (this repo will be Docker only)

--- src/platform/avr/builder/avr-atmega328p-20mhz.cmake
+++
@@ -1,7 +0,0 @@
-
-set(AVR_MCU     atmega328p  CACHE STRING "AVR MCU Type")
-set(AVR_CLOCK   20000000    CACHE STRING "AVR CPU Clock speed (Hz)")
-
-# ------------------------------------------------------------------------------
-
-set(AVRDUDE_PROGRAMMER  usbtiny CACHE STRING "")
