8741131375c9b53179faa3b3442059dc29ccc67e
Francois Best <francoisbest@gmail.com>
2017-04-06 12:13:44 +0200

    Cleanup (not really needed..)

--- scripts/docker-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${0}")/.." # cd to repository root
-
-NAME="fortyseveneffects/avr-builder"
-VERSION="$(cat version)"
-
-echo "Building $NAME:$VERSION"
-docker build -t $NAME:$VERSION -t $NAME:latest "$@" .
