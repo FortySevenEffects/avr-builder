80fd4c54657372fd879fa37b0281c7b6e9e98f14
Francois Best <francoisbest@gmail.com>
2017-04-07 08:59:59 +0200

    Configure at builder setup time instead of using scripts.

--- platform/avr/builder/avr-attiny84-20mhz.cmake
+++
@@ -1,7 +0,0 @@
-
-set(AVR_MCU     attiny84    CACHE STRING "AVR MCU Type")
-set(AVR_CLOCK   20000000    CACHE STRING "AVR CPU Clock speed (Hz)")
-
-# ------------------------------------------------------------------------------
-
-set(AVRDUDE_PROGRAMMER  usbtiny CACHE STRING "")
